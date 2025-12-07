bd49d906994cbf82baac23561dbecb75b5affa95
AOISYNM <pandeypnjn@gmail.com>
2025-12-07 19:53:18 +0545

    Added Merge Function

--- Code/main.py
+++ Code/main.py
@@ -1,8 +1,2 @@
-import matplotlib.pyplot as plt
-def plot_data(x, y):
-    plt.plot(x, y)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Sample Plot')
-    plt.show()
-plot_data([1, 2, 3], [4, 5, 6])
+no = int(input("enter your number"))
+print(no)
